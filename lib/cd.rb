5fe26efaf12fd20027e812c7d6f18f1b172c72c9
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-01 22:48:46 +0800

    Change cd.rb

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,59 +1,13 @@
 require_relative 'item.rb'
 
 class Cd < Item
-  def initialize(name, price)
+
+attr_accessor :name, :price, :description
+attr_reader :tracks, :artist, :run_time, :quantity
+
+  def initialize(name, price, tracks, artist, run_time)
     super(name, price)
     @tracks = tracks
     @artist = artist
     @run_time = run_time
   end
-
-
-  def tracks
-
-  end
-
-  def artist
-
-  end
-
-  def run_time
-
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-# class Item
-#   attr_reader :quantity
-#   attr_accessor :name, :price, :description
-#
-#   def initialize(name, price)
-#     @name = name
-#     @price = price
-#     @quantity = 0
-#     @description = ''
-#   end
-#
-#   def sell(amount)
-#     if @quantity >= amount
-#       @quantity -= amount
-#       true
-#     else
-#       false
-#     end
-#   end
-#
-#   def stock(amount)
-#     @quantity += amount
-#     true
-#   end
-# end
